ff306f3f66b147ca653b5b024f65639c5bb3c112
ALI <ali123rahiqi@gmail.com>
2025-04-08 17:31:38 +0100

    done

--- command.sh
+++ command.sh
@@ -1,2 +1,15 @@
 docker exec -it -u airflow airflow bash
 pip install -r /project/requirements.txt
+
+Créer_topic
+docker exec -it kafka kafka-topics --create --topic test --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+
+Publier_message_topic
+docker exec -it kafka kafka-console-producer --broker-list localhost:9092 --topic test
+
+Consommer_messages_topic
+docker exec -it kafka kafka-console-consumer --bootstrap-server localhost:9092 --topic test --from-beginning
+
+
+
+docker volume rm ecomdata_pipeline_kafka_data ecomdata_pipeline_zookeeper_data
